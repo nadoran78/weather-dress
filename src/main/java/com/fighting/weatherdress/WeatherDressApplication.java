8fa213baf11d470893d504e0a58a238e7eb4eb39
nadoran78 <nadoran78@gmail.com>
2023-12-14 10:54:32 +0900

    Initial commit

---
+++ src/main/java/com/fighting/weatherdress/WeatherDressApplication.java
@@ -0,0 +1,13 @@
+package com.fighting.weatherdress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDressApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WeatherDressApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/fighting/weatherdress/WeatherDressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fighting.weatherdress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
