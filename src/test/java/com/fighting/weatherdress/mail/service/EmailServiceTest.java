af24d9a38cfe18b35edc43c8234e6906f3aef372
nadoran78 <nadoran78@gmail.com>
2023-12-28 20:29:57 +0900

    test : 인증 실패 테스트 추가

--- src/test/java/com/fighting/weatherdress/mail/service/EmailServiceTest.java
+++ src/test/java/com/fighting/weatherdress/mail/service/EmailServiceTest.java
@@ -1,15 +1,18 @@
 package com.fighting.weatherdress.mail.service;
 
 import jakarta.mail.MessagingException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 // 일회성 테스트로 disable 처리함
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@Disabled
 class EmailServiceTest {
 
   @Autowired
@@ -29,11 +32,22 @@ class EmailServiceTest {
   void successVerifiedEmail() {
     //given
     String email = "nadoran78@gmail.com";
-    String code = "szmvFJ";
+    String code = "Xy1xWU";
     //when
-    Boolean b = emailService.verifiedEmail(email, code);
+    Boolean result = emailService.verifiedEmail(email, code);
     //then
-    System.out.println(b);
+    Assertions.assertEquals(result, true);
+  }
 
+  @Test
+  @DisplayName("인증코드 불일치 시 실패 테스트")
+  void failVerifiedEmail() {
+    // given
+    String email = "nadoran78@gmail.com";
+    String code = "1111";
+    // when
+    Boolean result = emailService.verifiedEmail(email, code);
+    // then
+    Assertions.assertEquals(result, false);
   }
 }
