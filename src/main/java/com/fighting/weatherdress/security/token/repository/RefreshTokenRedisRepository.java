3226fb6d4a847e817f838f91fa86f6b2bcddd9f6
nadoran78 <nadoran78@gmail.com>
2023-12-29 22:56:46 +0900

    chore : RefreshTokenRedisRepository 중복된 메서드 제거

--- src/main/java/com/fighting/weatherdress/security/token/repository/RefreshTokenRedisRepository.java
+++ src/main/java/com/fighting/weatherdress/security/token/repository/RefreshTokenRedisRepository.java
@@ -1,9 +1,8 @@
 package com.fighting.weatherdress.security.token.repository;
 
 import com.fighting.weatherdress.security.token.entity.RefreshToken;
-import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RefreshTokenRedisRepository extends CrudRepository<RefreshToken, String> {
-  Optional<RefreshToken> findById(String id);
+
 }
