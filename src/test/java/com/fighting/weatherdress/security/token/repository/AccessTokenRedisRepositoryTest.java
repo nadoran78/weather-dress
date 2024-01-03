1f71d465f0e07bafed73702cc68e9fead859bf0f
nadoran78 <nadoran78@gmail.com>
2024-01-03 22:34:51 +0900

    refactor : redis에 accessToken 저장 시 만료시간 가능하도록 변경

--- src/test/java/com/fighting/weatherdress/security/token/repository/AccessTokenRedisRepositoryTest.java
+++ src/test/java/com/fighting/weatherdress/security/token/repository/AccessTokenRedisRepositoryTest.java
@@ -1,13 +1,14 @@
 package com.fighting.weatherdress.security.token.repository;
 
 import com.fighting.weatherdress.security.token.entity.AccessToken;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -15,11 +16,6 @@ class AccessTokenRedisRepositoryTest {
 
   @Autowired
   private AccessTokenRedisRepository accessTokenRedisRepository;
-
-  @AfterEach
-  void rollBack() {
-    accessTokenRedisRepository.deleteAll();
-  }
 
   @Test
   void successExistsByToken() {
@@ -37,5 +33,22 @@ class AccessTokenRedisRepositoryTest {
     accessTokenRedisRepository.save(accessToken2);
     //then
     Assertions.assertTrue(accessTokenRedisRepository.existsByToken(token1));
+    accessTokenRedisRepository.deleteAll();
+  }
+
+  @Test
+  @Transactional
+  @DisplayName("생명주기 확인 테스트")
+  void expireAccessToken() throws InterruptedException {
+    String token = "aaaaaaaa";
+    long expiration = 3L;
+    AccessToken accessToken = AccessToken.builder().id("1111@abcd.com")
+        .token(token)
+        .expiration(expiration)
+        .build();
+    accessTokenRedisRepository.save(accessToken);
+    Assertions.assertTrue(accessTokenRedisRepository.existsByToken(token));
+    Thread.sleep(5 * 1000);
+    Assertions.assertFalse(accessTokenRedisRepository.existsByToken(token));
   }
 }
