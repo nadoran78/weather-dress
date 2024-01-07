0e5a89573b3b5db46d29f52f5a86568ae411debc
nadoran78 <nadoran78@gmail.com>
2024-01-07 22:51:17 +0900

    feat : 위경도에서 기상청 x,y 좌표로 변환 기능 구현

---
+++ src/main/java/com/fighting/weatherdress/weather/dto/CoordinateDto.java
@@ -0,0 +1,20 @@
+package com.fighting.weatherdress.weather.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CoordinateDto {
+  private double lat;
+  private double lng;
+
+  private int x;
+  private int y;
+}
