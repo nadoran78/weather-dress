f75dbad183fb501e56d2d797a94f04be5d70513a
nadoran78 <nadoran78@gmail.com>
2024-01-09 19:10:22 +0900

    feat : WeeklyWeather엔티티 구성 및 Location 엔티티와 연결

--- src/main/java/com/fighting/weatherdress/global/entity/Location.java
+++ src/main/java/com/fighting/weatherdress/global/entity/Location.java
@@ -1,10 +1,15 @@
 package com.fighting.weatherdress.global.entity;
 
+import com.fighting.weatherdress.weather.entity.WeeklyWeather;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -36,6 +41,13 @@ public class Location extends BaseEntity{
   private int yCoordinate; // 기상청 y 좌표
 
   @Column(nullable = false)
-  private String locationCode;
+  private String locationCode;  // 미시 예보구역코드(시군구 레벨의 예보지역코드) : 중기 온도예보 조회 시 필요
+
+  @Column(nullable = false)
+  private String locationLandCode;  // 거시 예보구역코드(시도 레벨의 예보지역코드) : 중기 날씨예보 조회 시 필요
+
+  @Builder.Default
+  @OneToMany(mappedBy = "location", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<WeeklyWeather> weeklyWeathers = new ArrayList<>();
 
 }
