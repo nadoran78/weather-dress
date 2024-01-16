87240358617d295900b39f278491acf579844c6e
nadoran78 <nadoran78@gmail.com>
2024-01-16 12:27:35 +0900

    fix : baseEntity 적용

--- src/main/java/com/fighting/weatherdress/post/entity/Post.java
+++ src/main/java/com/fighting/weatherdress/post/entity/Post.java
@@ -1,7 +1,10 @@
 package com.fighting.weatherdress.post.entity;
 
+import com.fighting.weatherdress.global.entity.BaseEntity;
 import com.fighting.weatherdress.global.entity.Location;
 import com.fighting.weatherdress.member.domain.Member;
+import com.fighting.weatherdress.s3.entity.Image;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,17 +13,22 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.envers.AuditOverride;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
-public class Post {
+@AuditOverride(forClass = BaseEntity.class)
+public class Post extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -46,4 +54,7 @@ public class Post {
   @JoinColumn(name = "location_id")
   private Location location;
 
+  @Builder.Default
+  @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Image> images = new ArrayList<>();
 }
