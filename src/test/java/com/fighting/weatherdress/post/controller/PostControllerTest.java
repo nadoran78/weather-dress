099a058c8c6edacfd9f73f37998721840a023c3d
nadoran78 <nadoran78@gmail.com>
2024-03-13 11:03:56 +0900

    test: post 등록 기능 구현 test

---
+++ src/test/java/com/fighting/weatherdress/post/controller/PostControllerTest.java
@@ -0,0 +1,124 @@
+package com.fighting.weatherdress.post.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fighting.weatherdress.config.WithMockCustomUser;
+import com.fighting.weatherdress.post.dto.PostRequest;
+import com.fighting.weatherdress.post.service.PostService;
+import com.fighting.weatherdress.security.filter.JwtAuthenticationFilter;
+import com.fighting.weatherdress.weather.dto.LocationDto;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.multipart.MultipartFile;
+import util.MockDataUtil;
+
+@WebMvcTest(controllers = PostController.class, excludeFilters = {
+    @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JwtAuthenticationFilter.class)})
+class PostControllerTest {
+
+  @MockBean
+  private PostService postService;
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  @WithMockCustomUser
+  void successRegisterPost() throws Exception {
+    //given
+    LocationDto locationDto = LocationDto.builder()
+        .sido("경기도")
+        .sigungu("군포시")
+        .build();
+    PostRequest request = PostRequest.builder()
+        .content("오늘 이렇게 입었어요")
+        .location(locationDto)
+        .build();
+
+    MockMultipartHttpServletRequestBuilder post = MockMvcRequestBuilders
+        .multipart(POST, "/post")
+        .part(MockDataUtil.createMockPart("request", request));
+
+    for (int i = 1; i <= 3; i++) {
+      MockMultipartFile mockMultipartFile = MockDataUtil.createMockMultipartFile("images",
+          "src/test/resources/image/image" + i + ".png");
+      post.file(mockMultipartFile);
+    }
+
+    //when
+    ResultActions resultActions = mockMvc.perform(post.with(csrf()))
+        .andDo(print());
+
+    //then
+    resultActions.andExpect(status().isOk());
+
+    ArgumentCaptor<PostRequest> postRequestArgumentCaptor = ArgumentCaptor.forClass(
+        PostRequest.class);
+    ArgumentCaptor<List<MultipartFile>> listArgumentCaptor = ArgumentCaptor.forClass(
+        List.class);
+    ArgumentCaptor<Long> longArgumentCaptor = ArgumentCaptor.forClass(Long.class);
+
+    verify(postService).registerPost(postRequestArgumentCaptor.capture(),
+        listArgumentCaptor.capture(), longArgumentCaptor.capture());
+
+    assertEquals(request.getContent(), postRequestArgumentCaptor.getValue().getContent());
+    assertEquals(request.getLocation().getSido(),
+        postRequestArgumentCaptor.getValue().getLocation().getSido());
+    assertEquals(request.getLocation().getSigungu(),
+        postRequestArgumentCaptor.getValue().getLocation().getSigungu());
+    assertEquals(3, listArgumentCaptor.getValue().size());
+    assertEquals("image1.png",
+        listArgumentCaptor.getValue().get(0).getOriginalFilename());
+    assertEquals("image2.png",
+        listArgumentCaptor.getValue().get(1).getOriginalFilename());
+    assertEquals("image3.png",
+        listArgumentCaptor.getValue().get(2).getOriginalFilename());
+    assertEquals(1, longArgumentCaptor.getValue());
+
+  }
+
+  @Test
+  @WithMockCustomUser
+  void registerPost_shouldValidException_whenInvalidRequest()
+      throws Exception {
+    //given
+    PostRequest request = PostRequest.builder()
+        .content("1".repeat(301))
+        .build();
+    //when
+    MockMultipartHttpServletRequestBuilder post = MockMvcRequestBuilders
+        .multipart(POST, "/post")
+        .part(MockDataUtil.createMockPart("request", request));
+
+    for (int i = 1; i <= 3; i++) {
+      MockMultipartFile mockMultipartFile = MockDataUtil.createMockMultipartFile("images",
+          "src/test/resources/image/image" + i + ".png");
+      post.file(mockMultipartFile);
+    }
+
+    //when & then
+    mockMvc.perform(post.with(csrf()))
+        .andDo(print())
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.location").exists())
+        .andExpect(jsonPath("$.content").exists());
+
+  }
+
+}
---
+++ src/test/java/com/fighting/weatherdress/post/service/PostServiceTest.java
@@ -0,0 +1,178 @@
+package com.fighting.weatherdress.post.service;
+
+import static com.fighting.weatherdress.global.type.ErrorCode.MEMBER_NOT_FOUND;
+import static com.fighting.weatherdress.global.type.ErrorCode.NOT_FOUND_LOCATION;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.fighting.weatherdress.global.entity.Location;
+import com.fighting.weatherdress.global.exception.CustomException;
+import com.fighting.weatherdress.global.repository.LocationRepository;
+import com.fighting.weatherdress.member.domain.Member;
+import com.fighting.weatherdress.member.repository.MemberRepository;
+import com.fighting.weatherdress.post.dto.PostRequest;
+import com.fighting.weatherdress.post.entity.Image;
+import com.fighting.weatherdress.post.entity.Post;
+import com.fighting.weatherdress.post.repository.ImageRepository;
+import com.fighting.weatherdress.post.repository.PostRepository;
+import com.fighting.weatherdress.s3.dto.S3FileDto;
+import com.fighting.weatherdress.s3.service.S3FileService;
+import com.fighting.weatherdress.weather.dto.DailyShortWeather;
+import com.fighting.weatherdress.weather.dto.LocationDto;
+import com.fighting.weatherdress.weather.dto.ShortTermWeatherResponse;
+import com.fighting.weatherdress.weather.service.ShortTermWeatherService;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
+import util.MockDataUtil;
+
+@ExtendWith(MockitoExtension.class)
+class PostServiceTest {
+
+  @Mock
+  private ShortTermWeatherService shortTermWeatherService;
+  @Mock
+  private PostRepository postRepository;
+  @Mock
+  private MemberRepository memberRepository;
+  @Mock
+  private S3FileService fileService;
+  @Mock
+  private LocationRepository locationRepository;
+  @Mock
+  private ImageRepository imageRepository;
+  @InjectMocks
+  private PostService postService;
+
+  private final PostRequest registerRequest = PostRequest.builder()
+      .content("이렇게 입었어요.")
+      .location(LocationDto.builder()
+          .sido("경기도")
+          .sigungu("군포시")
+          .build())
+      .build();
+
+  @Test
+  void successRegisterPost() throws URISyntaxException, IOException {
+    //given
+    Member member = Member.builder()
+        .id(1L)
+        .nickName("꼬북이")
+        .build();
+    Location location = Location.builder()
+        .sido("경기도")
+        .sigungu("군포시")
+        .build();
+    DailyShortWeather dailyShortWeather = DailyShortWeather.builder()
+        .date("20240313")
+        .maxTemperature(12)
+        .minTemperature(9)
+        .build();
+    ShortTermWeatherResponse response = ShortTermWeatherResponse.builder()
+        .sido("경기도")
+        .sigungu("군포시")
+        .today(dailyShortWeather)
+        .build();
+    List<S3FileDto> s3FileDtos = new ArrayList<>();
+    for (int i = 1; i <= 3; i++) {
+      S3FileDto s3FileDto = S3FileDto.builder()
+          .url(Integer.toString(i))
+          .build();
+      s3FileDtos.add(s3FileDto);
+    }
+
+    given(memberRepository.findById(anyLong())).willReturn(Optional.of(member));
+    given(locationRepository.findBySidoAndSigungu(anyString(), anyString()))
+        .willReturn(Optional.of(location));
+    given(shortTermWeatherService.getWeatherFromApi(anyString(), anyString()))
+        .willReturn(response);
+    given(postRepository.save(any(Post.class))).will(returnsFirstArg());
+    given(fileService.saveFile(any(List.class))).willReturn(s3FileDtos);
+    //when
+    postService.registerPost(registerRequest, createImageList(3), 1L);
+
+    //then
+    ArgumentCaptor<Post> postArgumentCaptor = ArgumentCaptor.forClass(Post.class);
+    ArgumentCaptor<Image> imageArgumentCaptor = ArgumentCaptor.forClass(Image.class);
+
+    verify(postRepository).save(postArgumentCaptor.capture());
+    verify(imageRepository, times(3)).save(imageArgumentCaptor.capture());
+
+    assertEquals(registerRequest.getContent(), postArgumentCaptor.getValue().getText());
+    assertEquals("경기도", postArgumentCaptor.getValue().getLocation().getSido());
+    assertEquals("군포시", postArgumentCaptor.getValue().getLocation().getSigungu());
+    assertEquals(9, postArgumentCaptor.getValue().getMinTemperature());
+    assertEquals(12, postArgumentCaptor.getValue().getMaxTemperature());
+    assertEquals("꼬북이", postArgumentCaptor.getValue().getMember().getNickName());
+
+    assertEquals("1", imageArgumentCaptor.getAllValues().get(0).getUrl());
+    assertEquals("2", imageArgumentCaptor.getAllValues().get(1).getUrl());
+    assertEquals("3", imageArgumentCaptor.getAllValues().get(2).getUrl());
+    assertEquals(postArgumentCaptor.getValue(),
+        imageArgumentCaptor.getAllValues().get(0).getPost());
+    assertEquals(postArgumentCaptor.getValue(),
+        imageArgumentCaptor.getAllValues().get(1).getPost());
+    assertEquals(postArgumentCaptor.getValue(),
+        imageArgumentCaptor.getAllValues().get(2).getPost());
+  }
+
+  @Test
+  void registerPost_shouldThrowMemberNotFound_whenMemberIsNotExist() {
+    //given
+    given(memberRepository.findById(anyLong())).willReturn(Optional.empty());
+
+    //when
+    CustomException customException = assertThrows(CustomException.class,
+        () -> postService.registerPost(registerRequest, createImageList(3), 1L));
+
+    //then
+    assertEquals(MEMBER_NOT_FOUND, customException.getErrorCode());
+  }
+
+  @Test
+  void registerPost_shouldThrowNotFoundLocation_whenLocationIsNotExist() {
+    //given
+    Member member = Member.builder()
+        .id(1L)
+        .nickName("꼬북이")
+        .build();
+
+    given(memberRepository.findById(anyLong())).willReturn(Optional.of(member));
+    given(locationRepository.findBySidoAndSigungu(anyString(), anyString()))
+        .willReturn(Optional.empty());
+
+    //when
+    CustomException customException = assertThrows(CustomException.class,
+        () -> postService.registerPost(registerRequest, createImageList(3), 1L));
+
+    //then
+    assertEquals(NOT_FOUND_LOCATION, customException.getErrorCode());
+  }
+
+  private List<MultipartFile> createImageList(int size) throws IOException {
+    List<MultipartFile> images = new ArrayList<>();
+    for (int i = 1; i <= size; i++) {
+      images.add(MockDataUtil.createMockMultipartFile("images",
+          "src/test/resources/image/image" + i + ".png"));
+    }
+    return images;
+  }
+
+
+}
