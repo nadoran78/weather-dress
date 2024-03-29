ed443038e7366eb0a38b82811e5e928827f35f14
nadoran78 <nadoran78@gmail.com>
2024-03-29 22:46:33 +0900

    chore: unuse import 삭제

--- src/main/java/com/fighting/weatherdress/post/entity/Image.java
+++ src/main/java/com/fighting/weatherdress/post/entity/Image.java
@@ -1,7 +1,6 @@
 package com.fighting.weatherdress.post.entity;
 
 import com.fighting.weatherdress.global.entity.BaseEntity;
-import com.fighting.weatherdress.post.entity.Post;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
