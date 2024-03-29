03ae0acfa4a5a2bc6f48795a57c6c027f9e8406c
nadoran78 <nadoran78@gmail.com>
2024-03-29 22:47:15 +0900

    feat: Reply Entity 및 Repository 구성

---
+++ src/main/java/com/fighting/weatherdress/reply/entity/Reply.java
@@ -0,0 +1,35 @@
+package com.fighting.weatherdress.reply.entity;
+
+import com.fighting.weatherdress.global.entity.BaseEntity;
+import com.fighting.weatherdress.member.domain.Member;
+import com.fighting.weatherdress.post.entity.Post;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(force = true, access = AccessLevel.PROTECTED)
+public class Reply extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String text;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id")
+  private Post post;
+}
---
+++ src/main/java/com/fighting/weatherdress/reply/repository/ReplyRepository.java
@@ -0,0 +1,10 @@
+package com.fighting.weatherdress.reply.repository;
+
+import com.fighting.weatherdress.reply.entity.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+}
