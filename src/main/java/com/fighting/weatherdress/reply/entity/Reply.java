79f89f32df8f8c7345132d9d3807022d860f6adf
nadoran78 <nadoran78@gmail.com>
2024-03-29 23:23:43 +0900

    feat: Reply register 기능 구현

--- src/main/java/com/fighting/weatherdress/reply/entity/Reply.java
+++ src/main/java/com/fighting/weatherdress/reply/entity/Reply.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -32,4 +33,12 @@ public class Reply extends BaseEntity {
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "post_id")
   private Post post;
+
+  @Builder
+  public Reply(String text, Member member, Post post) {
+    this.text = text;
+    this.member = member;
+    this.post = post;
+  }
+
 }
