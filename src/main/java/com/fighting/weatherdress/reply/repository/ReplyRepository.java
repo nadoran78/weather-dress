f226f8995a64aa770ec2231137d74c9d08350d32
nadoran78 <nadoran78@gmail.com>
2024-04-03 22:22:55 +0900

    feat: 게시글 별 답글 리스트 조회 기능 구현

--- src/main/java/com/fighting/weatherdress/reply/repository/ReplyRepository.java
+++ src/main/java/com/fighting/weatherdress/reply/repository/ReplyRepository.java
@@ -1,10 +1,13 @@
 package com.fighting.weatherdress.reply.repository;
 
+import com.fighting.weatherdress.post.entity.Post;
 import com.fighting.weatherdress.reply.entity.Reply;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
-
+  List<Reply> findAllByPost(Post post, Pageable pageable);
 }
